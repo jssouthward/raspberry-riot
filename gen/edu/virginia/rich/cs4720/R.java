00f79190448f63433d507dd794d01b5723a611f6
Jarred <jarred.higgs@gmail.com>
2014-11-21 21:44:06 -0500

    Title screen

--- gen/edu/virginia/rich/cs4720/R.java
+++ gen/edu/virginia/rich/cs4720/R.java
@@ -27,27 +27,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int IPfield=0x7f080001;
-        public static final int action_settings=0x7f080014;
-        public static final int button=0x7f080007;
-        public static final int domain=0x7f080004;
-        public static final int editText=0x7f080003;
-        public static final int game_parent_layout=0x7f080008;
+        public static final int IPfield=0x7f080002;
+        public static final int action_settings=0x7f080015;
+        public static final int button=0x7f080008;
+        public static final int domain=0x7f080005;
+        public static final int editText=0x7f080004;
+        public static final int game_parent_layout=0x7f080009;
         public static final int linearLayout1=0x7f080000;
-        public static final int p1Indicator=0x7f080012;
-        public static final int p1plus1=0x7f08000d;
-        public static final int p1plus2=0x7f080010;
-        public static final int p2Indicator=0x7f080013;
-        public static final int p2plus1=0x7f08000e;
-        public static final int p2plus2=0x7f080011;
-        public static final int player1Name=0x7f080005;
-        public static final int player2Name=0x7f080006;
-        public static final int progressBar1=0x7f08000a;
-        public static final int tableRow1=0x7f080009;
-        public static final int tableRow2=0x7f08000c;
-        public static final int tableRow3=0x7f08000f;
-        public static final int tableRow4=0x7f08000b;
-        public static final int textView=0x7f080002;
+        public static final int p1Indicator=0x7f080013;
+        public static final int p1plus1=0x7f08000e;
+        public static final int p1plus2=0x7f080011;
+        public static final int p2Indicator=0x7f080014;
+        public static final int p2plus1=0x7f08000f;
+        public static final int p2plus2=0x7f080012;
+        public static final int player1Name=0x7f080006;
+        public static final int player2Name=0x7f080007;
+        public static final int progressBar1=0x7f08000b;
+        public static final int tableRow1=0x7f08000a;
+        public static final int tableRow2=0x7f08000d;
+        public static final int tableRow3=0x7f080010;
+        public static final int tableRow4=0x7f08000c;
+        public static final int textView=0x7f080003;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_game_panel=0x7f030000;
